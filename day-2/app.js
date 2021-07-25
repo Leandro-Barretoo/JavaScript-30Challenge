65fe8f59bd99005041d69f6209af2ae6bf7f6d93
Leandro <officialdevmi@gmail.com>
2021-07-25 04:10:56 -0700

    Add hour pointer feature

--- day-2/app.js
+++ day-2/app.js
@@ -1,5 +1,6 @@
 const secondHand = document.querySelector('.second-hand');
 const minuteHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
 
 function setDate() {
   const now = new Date();
@@ -10,6 +11,10 @@ function setDate() {
   const minutes = now.getMinutes();
   const minutesDegrees = ((minutes / 60) * 360) + 90;
   minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+
+  const hours = now.getHours();
+  const hoursDegrees = ((hours / 12) * 360) + 90;
+  hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
 }
 
 setInterval(setDate, 1000);
